0c5f4748a1512d52f33665a5f36063c592ea6712
Elena5746328 <lenae.efimowa@yandex.ru>
2026-07-01 20:41:41 +0300

    cw: добавление новых страниц от 01.07.2026

--- users/views.py
+++ users/views.py
@@ -1,3 +1,81 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import login
+from django.contrib.auth.forms import AuthenticationForm
+from django.shortcuts import redirect, render
+from django.views import View
 
-# Create your views here.
+from .forms import ReqisterForm
+
+class LoginView(View):
+    template_name = "users/login.html"
+
+    def get(self, request):
+        return render(
+            request,
+            self.template_name, 
+            {
+                "username_value": "",
+                "errors": {},
+                "non_field_errors": []
+            }
+        )
+
+    def post(self, request):
+        form = AuthenticationForm(request, data=request.POST)
+
+        if not form.is_valid():
+            return render(
+                request,
+                self.template_name, 
+                {
+                    "username_value": request.POST.get("username", "").strip(),
+                    "errors": form.errors,
+                    "non_field_errors": form.non_field_errors()
+                }
+            )
+
+        
+        login(request, form.get_user())
+        messages.success(request, "Вход выполнен")
+        return redirect("/")
+
+class ReqisterView(View):
+    template_name = "users/reqister.html"
+
+    def get(self, request):
+            return render(
+                request,
+                self.template_name, 
+                {
+                    "username_value": "",
+                    "email_value": "",
+                    "errors": {},
+                    "non_field_errors": []
+                }
+            )
+
+    def post(self, request):
+        form = ReqisterForm(request, POST)
+
+        if not form.is_valid():
+            return render(
+                request,
+                self.template_name, 
+                {
+                    "username_value": request.POST.get("username", "").strip(),
+                    "email_value": request.POST.get("email", "").strip(),
+                    "errors": form.errors,
+                    "non_field_errors": form.non_field_errors
+                }
+            )
+
+        user = form.save()
+        login(request, user)
+        messages.success(request, "Регистрация выполнена")
+        return redirect("/")
+
+
+
+
+
+
